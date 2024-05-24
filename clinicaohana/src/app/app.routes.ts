db9a43c7681bc6a38d854aefcd5c17c242b914ce
lorenadp10 <lorenadiez@ymail.com>
2024-05-24 17:28:38 +0200

    ohana

--- clinicaohana/src/app/app.routes.ts
+++ clinicaohana/src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { ClinicaComponent } from './clinica/clinica.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '#clinica', component: ClinicaComponent },];
